bc1929217210bd01f44257e301689716c14453ff
SHREE <mansikothari46@gmail.com>
2024-12-30 17:30:46 +0530

    Add project files.

---
+++ FirstApp/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace FirstApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool running = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        {
+            running = !running;
+            if (running) {
+                tbStatus.Text = "Running....";
+                btnToggleRun.Content = "Stop";
+            } else
+            {
+                tbStatus.Text = "";
+                btnToggleRun.Content = "Run";
+            }
+           
+        }
+    }
+}
