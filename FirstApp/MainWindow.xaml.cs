9911d2c1c408de8dfbbacbe0375dc4c09f353e87
SHREE <mansikothari46@gmail.com>
2024-12-31 20:07:22 +0530

    Data Bindings

--- FirstApp/MainWindow.xaml.cs
+++ FirstApp/MainWindow.xaml.cs
@@ -1,18 +1,46 @@
-﻿using System.Windows;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
 
 namespace FirstApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
+            DataContext = this;
             InitializeComponent();
-
             
         }
 
+
+        private string boundText;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string BoundText
+        {
+            get { return boundText; }
+            set {
+                boundText = value;
+                // PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText"));
+                // OnPropertyChanged("BoundText");
+                OnPropertyChanged(); // Do not need to write the string bcuz of the CallerMemberName
+            }
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "Set from code!";
+
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
