91f6fb2d81f2077069953dd56d3fae2c1cdf847c
SHREE <mansikothari46@gmail.com>
2025-01-05 21:08:54 +0530

    List View Binding

--- FirstApp/MainWindow.xaml.cs
+++ FirstApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.ObjectModel;
 using System.Windows;
 
 namespace FirstApp
@@ -10,65 +11,38 @@ namespace FirstApp
     {
         public MainWindow()
         {
+            DataContext = this;
+            entries = new ObservableCollection<string>();
             InitializeComponent();
-            lvEntries.Items.Add("BMW");
         }
+
+        private ObservableCollection<string> entries; 
+
+        public ObservableCollection<string> Entries
+        {
+            get { return entries; }
+            set { entries = value; }
+        }
+
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-            if (txtEntry.Text != "")
-            {
-                lvEntries.Items.Add(txtEntry.Text);
+            if (txtEntry.Text != "") {
+                Entries.Add(txtEntry.Text);
                 txtEntry.Clear();
                 txtEntry.Focus();
-            } else
-            {
-                MessageBox.Show("Please enter a value", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                txtEntry.Focus();
             }
-
         }
 
         private void btnClear_Click(object sender, RoutedEventArgs e)
         {
-            lvEntries.Items.Clear();
+            Entries.Clear();
         }
 
         private void btnDel_Click(object sender, RoutedEventArgs e)
         {
-
-            //int selectedIndex = lvEntries.SelectedIndex;
-            //if (selectedIndex != -1)
-            //{
-            //    lvEntries.Items.RemoveAt(selectedIndex);
-            //} else
-            //{
-            //    MessageBox.Show("Please select an item to delete", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            //}
-
-            //object selectedItem = lvEntries.SelectedItem;
-            //if (selectedItem != null)
-            //{
-            //    MessageBoxResult result = MessageBox.Show($"Are you Sure you want to delete: {(string)selectedItem}?", "Delete Item", MessageBoxButton.YesNo, MessageBoxImage.Question);
-            //    if (result == MessageBoxResult.Yes)
-            //    {
-            //        lvEntries.Items.Remove(selectedItem);
-            //    }
-            //} else
-            //{
-            //    MessageBox.Show("Please select an item to delete", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            //}
-
-            var selectedItems = lvEntries.SelectedItems;//.Cast<object>().ToList();
-            MessageBoxResult result = MessageBox.Show($"Are you Sure you want to delete: {selectedItems.Count}?", "Delete Item", MessageBoxButton.YesNo, MessageBoxImage.Question);
-            if (result == MessageBoxResult.Yes) {
-                var itemsList = new ArrayList(selectedItems);
-                foreach (var item in itemsList) {
-                    lvEntries.Items.Remove(item);
-                }
-            }
-            txtEntry.Focus();
-
+            string item = (string)lvEntries.SelectedItem;
+            Entries.Remove(item);
         }
     }
 }
